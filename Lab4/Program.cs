76516d31eaad612a3b2b92bad5e91f75a6013153
Ana-Malina Pirvu <malinapirvu24@gmail.com>
2023-04-25 23:47:00 +0300

    remove comments

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,16 +1,16 @@
 ﻿namespace Lab4 {
     public class Program {
         public static void Main() {
-            // string[] lines = File.ReadAllLines("C:\\Users\\Admin\\RiderProjects\\CryptographyLab\\Lab4\\hw1.txt");
-            // string encText = String.Join("", lines);
+            string[] lines = File.ReadAllLines("C:\\Users\\Admin\\RiderProjects\\CryptographyLab\\Lab4\\hw1.txt");
+            string encText = String.Join("", lines);
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
-            // for (int i = 0; i < 26; i++) {
-            //     Console.WriteLine(i); //pt index 17
-            //     Console.WriteLine(new SimpleSubstitution().Encipher(alphabet,i));
-            //     Console.WriteLine(new SimpleSubstitution().Decipher(encText,i));
-            //     Console.WriteLine();
-            //     
-            // }
+            for (int i = 0; i < 26; i++) {
+                Console.WriteLine(i); //pt index 17
+                Console.WriteLine(new SimpleSubstitution().Encipher(alphabet,i));
+                Console.WriteLine(new SimpleSubstitution().Decipher(encText,i));
+                Console.WriteLine();
+                
+            }
 
             string key = "cryptography";
             string finalAlphabet = new Transposition().FinalAlphabet(key,3);
