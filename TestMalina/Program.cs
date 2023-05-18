804bc90d64fe4df32d5290de57c52de5f2d2f64e
Ana-Malina Pirvu <malinapirvu24@gmail.com>
2023-05-18 10:55:37 +0300

    test

--- TestMalina/Program.cs
+++ TestMalina/Program.cs
@@ -1,8 +1,29 @@
 ﻿namespace TestMalina {
     class Program {
+        
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(Help.AlphabetPermutation());
+            List<int> order = new List<int> { 2,1,5,0,6,4,3};
+
+            string result = Help.ReadByColumns("tiohnngteigoiemovatoegnzhnie", 4, order);
+            Console.WriteLine(result);
+
+            
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string text = "VHSHCKDOWOLFRKFKYFYYHYLH".ToLower();
+            string pp = Help.ReadByColumns(text, 4);
+            for (int i = 0; i < 26; i++) {
+                Console.WriteLine(i); //pt index 17
+                Console.WriteLine(Help.EncipherCaesar(alphabet,i));
+                Console.WriteLine(Help.DecipherCaesar(pp.,i));
+                Console.WriteLine();
+                
+            }
+            string text = "VHSHCKDOWOLFRKFKYFYYHYLH".ToLower();
+            Help.ReadByColumns(text, 4);
+
+
+
+
         }
     }
 }
